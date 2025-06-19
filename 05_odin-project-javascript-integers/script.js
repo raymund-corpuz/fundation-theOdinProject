02a9b094cd6c95f3c51ba4e7dd07195a5c15033e
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-06-19 21:38:41 +0800

    Math Equations: Fix the given variables - turn them into number datatype

--- 05_odin-project-javascript-integers/script.js
+++ 05_odin-project-javascript-integers/script.js
@@ -173,3 +173,11 @@ function numberChecker(number) {
     return false;
   }
 }
+
+//MATH EQUATIONS
+const a = 1 - 1; // Freebie!!! This is the answer to "one minus one"
+const b = 1 + 8;
+const c = 22 * 3;
+const d = 5 / 4;
+const e = b - 17;
+const f = a + b + c + d + e;
