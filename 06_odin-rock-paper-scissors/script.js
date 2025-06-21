2736fc9d11a4a9d203e8f8f712cacf15d1806d23
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-06-21 09:13:13 +0800

    06_odin-rock-paper-scissors : Step 1: use Math.rand() function this should return 1 - 3. Step 2: create an array of tools.Step 3: store the rock paper scissors inside the array. Step 4: pass the rand inside the array. Step 5: Create a varible to hold the computerChoice. Step 6 : return the computer choice

---
+++ 06_odin-rock-paper-scissors/script.js
@@ -0,0 +1,21 @@
+//create a function getCOmputerChoice
+
+function getComputerChoice() {
+  const computerChoice = Math.trunc(Math.random() * 3);
+  const tools = ["rock", "paper", "scissors"];
+  getUserChoice();
+  return tools[computerChoice];
+
+  // Step 1: use Math.rand() function this should return 1 - 3
+  //   const computerChoice = Math.trunc(Math.random() * 3);
+  //   const select = computerChoice;
+  //   // Step 2: create an array of tools
+  //   // Step 3: store the rock paper scissors inside the array
+  //   const tools = ["rock", "paper", "scissors"];
+  //   // Step 4: pass the rand inside the array
+  //   // Step 5: Create a varible to hold the computerChoice
+  //   // return the computer choice
+  //   console.log(tools[select]);
+  //   const finalSelect = tools[select];
+  //     return finalSelect;
+}
