6d5bd2c37916fd099619f3b8a7e614e371399ef7
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-06-23 07:59:41 +0800

    07_odin-loop-and-arrays : Use filter using query of string and return the result

--- 07_odin-loop-and-arrays/loops.js
+++ 07_odin-loop-and-arrays/loops.js
@@ -2,7 +2,7 @@ function isBigEnough(value) {
   return value > 10;
 }
 
-console.log([2, 4, 7, 9, 12, 15].filter(isBigEnough));
+// console.log([2, 4, 7, 9, 12, 15].filter(isBigEnough));
 
 const array1 = [-3, -2, -1, 0, 1, 3, 5, 7, 8, 9, 10, 11, 12, 13, 15, 17];
 
@@ -15,4 +15,14 @@ const isPrime = (number) => {
   return number > 1;
 };
 
-console.log(array1.filter(isPrime));
+// console.log(array1.filter(isPrime));
+
+const fruits = ["apples", "banana", "grapes", "mango", "orange"];
+
+function searchFilter(array, query) {
+  const result = array.filter((arr) => arr.includes(query));
+
+  console.log(result);
+}
+
+searchFilter(fruits, "ana");
