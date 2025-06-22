0ad28ad338aa4f4f197d23ae799ff0bfd2eb2d7b
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-06-23 07:47:12 +0800

    07_odin-loop-andarrays : Solving an array of Numbers if it is a prime number or not

--- 07_odin-loop-and-arrays/loops.js
+++ 07_odin-loop-and-arrays/loops.js
@@ -3,3 +3,16 @@ function isBigEnough(value) {
 }
 
 console.log([2, 4, 7, 9, 12, 15].filter(isBigEnough));
+
+const array1 = [-3, -2, -1, 0, 1, 3, 5, 7, 8, 9, 10, 11, 12, 13, 15, 17];
+
+const isPrime = (number) => {
+  for (let i = 2; number > i; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return number > 1;
+};
+
+console.log(array1.filter(isPrime));
