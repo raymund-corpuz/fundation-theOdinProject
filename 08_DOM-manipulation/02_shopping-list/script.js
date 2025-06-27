21ace71a45554e0217a55e6117e7cc8c885bd59f
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-06-27 15:14:11 +0800

    08_DOM-manipulation : Fix the Input Field. If no Input return an alert message.

--- 08_DOM-manipulation/02_shopping-list/script.js
+++ 08_DOM-manipulation/02_shopping-list/script.js
@@ -5,21 +5,26 @@ const div = document.querySelector("div");
 
 function runBtnClick() {
   const currentValue = inputUser.value;
-  inputUser.value = "";
-  console.log(currentValue);
 
-  const listLi = document.createElement("li");
-  const listSpan = document.createElement("span");
-  listSpan.textContent = currentValue;
-  listLi.appendChild(listSpan);
-  const delButton = document.createElement("button");
-  listLi.appendChild(delButton);
-  delButton.textContent = "delete";
-  delButton.addEventListener("click", () => {
-    listLi.remove();
-  });
-  itemList.appendChild(listLi);
-  inputUser.focus();
+  if (currentValue) {
+    inputUser.value = "";
+    console.log(currentValue);
+
+    const listLi = document.createElement("li");
+    const listSpan = document.createElement("span");
+    listSpan.textContent = currentValue;
+    listLi.appendChild(listSpan);
+    const delButton = document.createElement("button");
+    listLi.appendChild(delButton);
+    delButton.textContent = "delete";
+    delButton.addEventListener("click", () => {
+      listLi.remove();
+    });
+    itemList.appendChild(listLi);
+    inputUser.focus();
+  } else {
+    alert("No Input");
+  }
 }
 
 btnUser.addEventListener("click", runBtnClick);
