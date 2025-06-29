017d9828aff8e273115fb437f951a055f6681c3f
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-06-29 08:26:50 +0800

    06_odin-rock-paper-scissors: //step 1: store all buttons into a variable // A) Create a foreach loop to call PlayGame() //B ) call the getUserChoice(pass a variable here..)

--- 06_odin-rock-paper-scissors/script-rps-ui.js
+++ 06_odin-rock-paper-scissors/script-rps-ui.js
@@ -1,1 +1,13 @@
 //step 1: store all buttons into a variable
+// A) Create a foreach loop to call PlayGame()
+//B ) call the getUserChoice(pass a variable here..)
+const buttonCollection = document.querySelectorAll("button");
+buttonCollection.forEach((button) => {
+  button.addEventListener("click", () => {
+    const userInput = button.textContent;
+    playGame(userInput);
+  });
+});
+
+let userScore = 0;
+let computerScore = 0;
