5abab289c2813f095af054614631178208a0aef2
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-06-29 22:14:30 +0800

    09_odin-etch-a-sketch : Adding additional boxes into the container

--- 09_odin_etch-a-sketch/script.js
+++ 09_odin_etch-a-sketch/script.js
@@ -1,10 +1,20 @@
 const div = document.querySelector(".container");
-const divLength = 16;
+const colorPickerDiv = document.querySelector(".colorPickerDiv");
+const divLength = 32;
+
+div.style.width = `${divLength * 20 + divLength}px`;
+div.style.hegiht = `${divLength * 20 + divLength}px`;
+colorPickerDiv.style.width = `${divLength * 20 + divLength}px`;
 
 const colorPicker = document.querySelector("#colorPicker");
+colorPicker.addEventListener("change", (event) => {
+  const myColor = event.target.value;
 
-for (let i = 0; i <= divLength; i++) {
-  for (let j = 0; j <= divLength; j++) {
+  setColor(event.target.value || "#000000");
+});
+
+for (let i = 0; i < divLength; i++) {
+  for (let j = 0; j < divLength - 1; j++) {
     const innerDiv = document.createElement("div");
     innerDiv.classList.add("boxes");
     innerDiv.style.width = "20px";
@@ -12,11 +22,19 @@ for (let i = 0; i <= divLength; i++) {
     innerDiv.style.border = "1px solid black";
 
     div.appendChild(innerDiv);
-
-    innerDiv.addEventListener("mouseover", () => {
-      innerDiv.style.backgroundColor = "black";
-    });
   }
+}
+
+function setColor(color = "black") {
+  const innerDive = document.querySelectorAll(".boxes");
+  const innerDiv = [...innerDive];
+  innerDiv.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+      element.style.backgroundColor = color;
+    });
+  });
+
+  console.log(color);
 }
 
 const clear = document.querySelector(".clear");
