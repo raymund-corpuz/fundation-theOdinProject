68d345e973271195af867eaaa54d3c178ad5013b
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-01 09:30:46 +0800

    10_odin-object-bascis: Implementing the (new) keyword - Constructor function

--- 10_odin-object-basics/script2.js
+++ 10_odin-object-basics/script2.js
@@ -32,3 +32,48 @@ console.log(personObjectObject.name.first);
 console.log(personObjectObject.name.last);
 console.log(personObjectObject.name["first"]);
 console.log(personObjectObject["name"]["last"]);
+
+personObjectObject["eyes"] = "hazel";
+personObjectObject.farewell = function () {
+  return "Bye Everyone";
+};
+
+console.log(personObjectObject.eyes);
+console.log(personObjectObject.farewell());
+
+const personObjectMember = {};
+
+const myDataName = "height";
+const myDataValue = "1.75m";
+const myDataNameVar = [];
+
+personObjectMember[myDataNameVar] = myDataName;
+console.log(personObjectMember[myDataNameVar]);
+
+//CONSTRUCTOR =====================================>
+function createPersonConstructor(name) {
+  const obj = {};
+
+  obj.name = name;
+  obj.introduceSelf = function () {
+    return `Hi I'm ${this.name}`;
+  };
+  return obj;
+}
+
+console.log(createPersonConstructor("Raymund"));
+console.log(createPersonConstructor("Raymund").introduceSelf());
+
+const salva = createPersonConstructor("Salva");
+console.log(salva.introduceSelf());
+console.log(createPersonConstructor("frankie").introduceSelf());
+
+function PersonConstructor(name) {
+  this.name = name;
+  this.introduceSelf = function () {
+    return `Hi  I'm ${this.name}`;
+  };
+}
+
+const salvaConstructor = new PersonConstructor("Salva");
+console.log(salvaConstructor.introduceSelf());
